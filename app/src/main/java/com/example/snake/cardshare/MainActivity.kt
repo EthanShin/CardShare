f5a3020ae07d1818fc97b61e6cd08e400a5ecde1
EthanShin <avoirom@gmail.com>
2019-03-12 02:12:44 +0900

    fix: Fix an error when changing target sdk to 24
    The increased level of file access security offered by a content URI makes FileProvider a key part of Android's security infrastructure.
    
    1. Create the "provider_paths.xml" file.
    2. Add the provider to AndroidManifest.xml.
    3. Change "Uri.fromFile()" to "FileProvider.getUriForFile()".

--- app/src/main/java/com/example/snake/cardshare/MainActivity.kt
+++ app/src/main/java/com/example/snake/cardshare/MainActivity.kt
@@ -14,6 +14,7 @@ import java.io.FileOutputStream
 import android.support.v4.app.ActivityCompat
 import android.content.pm.PackageManager
 import android.os.Build
+import android.support.v4.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -58,7 +59,7 @@ class MainActivity : AppCompatActivity() {
     private fun grantExternalStoragePermission(): Boolean {
 
         // 마시멜로우 버전 이상...
-        return if (Build.VERSION.SDK_INT >= 23) {
+        return if (Build.VERSION.SDK_INT >= 24) {
             if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                 true
             } else {
@@ -146,7 +147,7 @@ class MainActivity : AppCompatActivity() {
     fun ShareImage(f: File) {
 
         // 파일정보를 uri로 만든다.
-        val uri = Uri.fromFile(f)
+        val uri = FileProvider.getUriForFile(this, "com.example.snake.cardshare.provider", f)
 
         // "image/*" type으로 지정하고 action은 Intent.ACTION_SEND인 Intent를 만든다.
         val intent = Intent()
